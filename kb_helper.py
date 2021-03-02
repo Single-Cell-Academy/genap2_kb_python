42d41c6b390e899f52868186103781a2f41ad0eb
JoelPHoward <joel.p.howard@gmail.com>
2021-03-02 20:17:19 +0000

    .

--- kb_helper.py
+++ kb_helper.py
@@ -1,8 +1,7 @@
 import anndata
 import pandas as pd
 
-f = "./kb_outs/counts_unfiltered/adata.loom"
-adata = anndata.read_loom(f)
+adata = anndata.read_loom("./kb_outs/counts_unfiltered/adata.loom")
 adata.var["gene_id"] = adata.var.index.values
 t2g = pd.read_csv("/cvmfs/soft.galaxy/v2.1/server/tools/single_cell_tools/refs/kb_python/Index/Mouse/t2g.txt", sep = "\t", header = None, names=["tid", "gene_id", "gene_name"])
 t2g.index = t2g.gene_id
@@ -13,4 +12,4 @@ adata.var.index = adata.var["Gene"]
 adata.var_names_make_unique()
 adata.obs["CellID"] = list(adata.obs.index)
 #adata.obs["ClusterID"] = [0] * adata.obs['CellID'].shape[0] #added to try to conform with loom validation
-adata.write_loom(f)
+adata.write("./kb_outs/counts_unfiltered/adata.h5ad")
